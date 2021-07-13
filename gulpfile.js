af3ab0e6050a2f56b5d37829f296d89aaaa8d612
Z3r0cks <patrick.kaserer@web.de>
2021-07-13 17:24:06 +0200

    ADD: gulp sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,22 @@
-function defaultTask(cb) {
-   // place code for your default task here
-   cb();
- }
- 
- exports.default = defaultTask
+const { src, dest, watch } = require('gulp');
+const compileSass = require('gulp-sass')(require('sass'));
+const minifyCss = require('gulp-clean-css')
+const sourceMaps = require('gulp-sourcemaps')
+
+compileSass.compiler = require('node-sass');
+
+const bundleSass = () => {
+   return src('./src/scss/**/*.scss')
+   .pipe(sourceMaps.init())
+      .pipe(compileSass().on('error', compileSass.logError))
+      .pipe(minifyCss())
+      .pipe(sourceMaps.write())
+      .pipe(dest('./dist/css/'));
+}
+
+const devWatch = () => {
+   watch('./src/scss/**/*.scss', bundleSass)
+}
+
+exports.bundleSass = bundleSass;
+exports.devWatch = devWatch;
---
+++ src/ts/main.js
@@ -0,0 +1,4 @@
+"use strict";
+function testFunction() {
+    console.log("Hello Worl");
+}
