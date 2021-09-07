12e408459830cf1bd1ea4984fc17e6cee64419a2
Z3r0cks <patrick.kaserer@web.de>
2021-09-07 14:26:08 +0200

    WIP: ....

--- src/server/HomePage.ts
+++ src/server/HomePage.ts
@@ -1,7 +1,8 @@
-import { HtmlBodyElement, HtmlButtonElement, HtmlDivElement, HtmlH3Element, HtmlHeaderElement, HtmlNavElement, HtmlPElement } from "./HtmlElement"
+import { HtmlBodyElement, HtmlButtonElement, HtmlDivElement, HtmlH1Element, HtmlHeaderElement, HtmlNavElement, HtmlPElement } from "./HtmlElement"
 import { Html } from "./createHtml"
 import { Page } from "./Page";
-import { TablePersonalWebsite } from "./interfaces/TablePersonalWebsite";
+import TablePersonalWebsite from "./interfaces/TablePersonalWebsite";
+import { HtmlElement } from "../../dist/server/HtmlElement";
 
 export class HomePage extends Page {
 
@@ -10,13 +11,16 @@ export class HomePage extends Page {
    }
 
    async buildPage() {
+      const list: HtmlDivElement = new HtmlDivElement("sdf", false, false, [new HtmlH1Element(false, false, false, false)])
       try {
          // const response: TablePersonalWebsite = await this.query("SELECT * FROM `content` WHERE `Name` = 'title_name'") as TablePersonalWebsite;
          // const titleName: string = response[0].Text_Content;
+         // const HTMLList: HtmlElement = this.getContent();
+         // console.log(HTMLList);
          this.html = new Html("de", "./css/style.css", "Mein Title", ["./js/main.js"]);
          this.header = new HtmlHeaderElement(false, false, false, [
             new HtmlNavElement("navbar", false, false, [
-               new HtmlH3Element("navTitle", false, "Patrick Kaserer", false),
+               new HtmlH1Element("navTitle", false, "Patrick Kaserer", false, "h1"),
                new HtmlDivElement(false, false, false, [
                   new HtmlButtonElement("btn btn--second", false, "über mich", false),
                   new HtmlButtonElement("btn btn--second", false, "Referenzen", false),
@@ -38,5 +42,22 @@ export class HomePage extends Page {
       return this.html.newHTML([
          this.body,
       ]);
+   }
+
+   private async getContent(): Promise<HtmlElement[]> {
+      const response: TablePersonalWebsite = await this.query("SELECT * FROM `content`") as TablePersonalWebsite;
+      const htmlList: HtmlElement[] = [];
+      console.log(htmlList);
+      response.forEach(async function (e) {
+         return htmlList.push(getHTMLType(e));
+      })
+
+      function getHTMLType(res) {
+         switch (res.DEV_NAME) {
+            case "c_title":
+               return new HtmlH1Element("c_title", false, "lorem Ipsum überschrift", false, "h1");
+         }
+      }
+      return htmlList;
    }
 }
